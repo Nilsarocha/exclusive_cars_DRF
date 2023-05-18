83ea777be52ed512ec0cdfdea4a9590f49aae67f
Nils <arochanils@gmail.com>
2023-05-18 15:57:31 +0100

    feature: view and serializer

--- car/serializers.py
+++ car/serializers.py
@@ -6,7 +6,13 @@ from .models import Car
 
 
 class CarSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Car
         fields = "__all__"
+
+
+class UpdateCarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = "__all__"
+        read_only_field = ["id", "mileage", "price"]
