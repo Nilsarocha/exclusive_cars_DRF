7a3f3e50101dc995dca6b7a2fdb2a9c96698e023
Nils <arochanils@gmail.com>
2023-06-19 22:32:37 +0100

    feature:  better-code in view

--- car/urls.py
+++ car/urls.py
@@ -2,10 +2,11 @@
 from rest_framework.routers import SimpleRouter
 
 # proof class import
-from .views import CarViewSet
+from .views import CarViewSet, MeCarView
 
 router = SimpleRouter()
 
 router.register(r'cars', CarViewSet, basename="cars")
+router.register(r'me/cars', MeCarView, basename="me_cars")
 
 urlpatterns = router.urls
